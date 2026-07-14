82931788548446e992a343b7b1999e0a8e807f50
OM <omjaiswal2552@gmail.com>
2026-07-14 11:58:55 +0530

    july-14

--- new-react-app/src/components/navbar/Navbar.jsx
+++ new-react-app/src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
+
 function Navbar() {
+
+    const {toggleTheme} = useContext(ThemeContext); 
 
     return (
         <div>
-            <p>This is Navbar Component</p>
+            {/* <p>This is Navbar Component</p> */}
+            <Link to='/'>Dashboard</Link>
+            {/* <Link to='/about'>About</Link> */}
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+
+            <button onClick={() => toggleTheme()}>Toggle</button>
         </div>
     )
 }
